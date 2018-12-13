8bb49c8b2fbd7c85e7e1b42f802d6f5767cf99c8
LC <lc@LCdeMacBook-Pro.local>
2018-12-13 14:35:21 +0800

    添加配置项

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from config import config
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    # 引入配置项
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
